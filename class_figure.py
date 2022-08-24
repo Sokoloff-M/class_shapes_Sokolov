ce178682bf3084ad60263874a0004b1fcbf41b28
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2022-08-24 14:05:43 +0800

    The circle class is described

--- class_figure.py
+++ class_figure.py
@@ -1,5 +1,8 @@
+import math
+
+
 class Rectangle:
-    def __init__(self, height: int, widght: int, sqare: int):
+    def __init__(self, height: int, widght: int,):
         self.widght = widght
         self.height = height
 
@@ -10,7 +13,18 @@ r1 = Rectangle(15, 43)
 print(r1.area())
 
 
-
 class Circle:
     def __init__(self, rad: int):
         self.rad = rad
+
+    def area(self):
+        return math.pi * (self.rad ** 2)
+
+    def len_Circle(self):
+        return 2 * math.pi * self.rad
+
+
+r2 = Circle(16)
+print(r2.area())
+print()
+print(r2.len_Circle())
