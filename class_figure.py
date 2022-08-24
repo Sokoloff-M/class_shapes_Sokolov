3e31a9a4475a66c2b29aab00b16844ff12b0d19e
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2022-08-24 13:50:07 +0800

    Выполнен класс Окружность

--- class_figure.py
+++ class_figure.py
@@ -2,8 +2,15 @@ class Rectangle:
     def __init__(self, height: int, widght: int, sqare: int):
         self.widght = widght
         self.height = height
-        self.sqare = sqare
+
+    def area(self):
+        return self.widght * self.height
+
+r1 = Rectangle(15, 43)
+print(r1.area())
+
 
 
 class Circle:
+    pass
     pass
